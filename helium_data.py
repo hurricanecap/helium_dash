76559c469f3800e44aad9071c982e685aab37132
hurricanecap <87330211+hurricanecap@users.noreply.github.com>
2021-08-17 13:08:16 -0400

    Update helium_data.py

--- helium_data.py
+++ helium_data.py
@@ -228,8 +228,6 @@ def add_total_avg(df):
     df = df.append(d_total, ignore_index = True)
     return df.loc[:, (df != 0).any(axis=0)]
 
-def get_quart():
-    pass
 if check_password():
     st.sidebar.write("## Helium Hotspots")
     total_earnings = sending_request('https://api.helium.io/v1/accounts/'+ nen +'/rewards/sum?min_time=2021-06-01T00:00:00')['sum']
@@ -244,15 +242,15 @@ if check_password():
         city_name = st.sidebar.selectbox('Choose a city' ,options)
         filt = st.sidebar.selectbox('Filter Online/Offline', ['All', 'Online','Offline'])
         hot_data = stats(city_name)
-        quantiles = hot_data[['total mined']].quantile(q=[.75,.5,.25, 0], axis= 0)
+        quantiles = hot_data[['month earnings']].quantile(q=[.75,.5,.25, 0], axis= 0)
         quantiles.index = ['1st quartile','2nd quartile','3rd quartile','4th quartile',]
-        quantiles.columns = ['earnings quartiles']
-        quantiles['earnings quartiles'] = quantiles.apply(lambda x: str(round(x['earnings quartiles'],2)), axis = 1)
+        quantiles.columns = ['month earnings quartiles']
+        quantiles['month earnings quartiles'] = quantiles.apply(lambda x: str(round(x['month earnings quartiles'],2)), axis = 1)
         quantiles
         
-        df = hot_data.set_index('name')[['total mined']]
-        quant = df['total mined'].quantile(q=[0,.25,.5,.75,1]).values
-        df['quartile'] = pd.cut(df['total mined'], quant, labels = ['4','3','2','1'])
+        df = hot_data.set_index('name')[['month earnings']]
+        quant = df['month earnings'].quantile(q=[0,.25,.5,.75,1]).values
+        df['quartile'] = pd.cut(df['month earnings'], quant, labels = ['4','3','2','1'])
         hot_data['quartile'] = hot_data['name'].map(df.to_dict()['quartile'])
         hot_data = add_total_avg(hot_data).set_index('name')
 
@@ -267,3 +265,4 @@ if check_password():
     if page == 'Earnings Data':
         cities = compiled().set_index('city')
         st.table(cities.style.apply(lambda x: ['background: lightsteelblue' if x.name == 'TOTAL' else '' for i in x], axis=1).set_precision(2))
+
